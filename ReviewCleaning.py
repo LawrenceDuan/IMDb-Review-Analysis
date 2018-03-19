5c98b65843c46ac4b2e996e7a0a00d1232bb0e3f
Cheng Duan <lawrence.chengduan@gmail.com>
2018-03-19 20:40:58 +0000

    Text cleaning.

---
+++ ReviewCleaning.py
@@ -0,0 +1,9 @@
+import re
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           text)
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emoticons).replace('-', ''))
+    return text
